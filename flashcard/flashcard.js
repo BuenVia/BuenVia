5a44e0d379eb629a8c2f346da171858178ced2dc
SaracensFan <matthewclifford@hotmail.co.uk>
2022-04-10 21:03:40 +0100

    yodate'

--- flashcard/flashcard.js
+++ flashcard/flashcard.js
@@ -12,7 +12,8 @@ const nextSubmit = id('nextSubmit')
 
 const summaryContainer = id('summaryContainer')
 
-let chosenCategory, wordIndex = 0, wrongAnswer = []
+let chosenCategory = [], wordIndex = 0
+let wrong = []
 
 // Shows available topics on dashboard
 col.forEach((cat) => {
@@ -28,9 +29,10 @@ col.forEach((cat) => {
 
     //Console Log each word with its category
     wordDiv.addEventListener('click', () => {
+        // Add property to each word in array to check if a correct answer has been given
         cat.words.forEach((i) => i.correct = false)
-        console.log(cat);
         chosenCategory = cat.words
+        console.log(chosenCategory);
         categoryEl.textContent = `${cat.category}`
         dashboardContainer.classList.add('hidden')
         flashcardContainer.classList.remove('hidden')
@@ -42,26 +44,30 @@ function openFlashcard() {
     nextSubmit.style.backgroundColor = ''
     // chosenCategory.forEach((word) => console.log(word))
     if (wordIndex < chosenCategory.length) {
-        console.log(wordIndex, wrongAnswer);
+        console.log(wordIndex);
         userInput.disabled = false
         nextSubmit.style.display = 'none'
         userInputSubmit.style.display = 'inline'
         wordEl.textContent = `${chosenCategory[wordIndex].spa}`
         userInputSubmit.addEventListener('click', checkAnswer)
     } else {
-        flashcardContainer.classList.add('hidden')
-        summaryContainer.classList.remove('hidden')
+        
+        console.log('--------END--------');
+        wrongAnswers()
     }
 }
 
 function checkAnswer() {
     if (userInput.value) {
+        chosenCategory[wordIndex].correct = true
+        console.log(chosenCategory[wordIndex]);
         nextSubmit.style.backgroundColor = 'green'
         resetValues()
     } else {
-        // Wrong answers are pushed to the 'wrongAnswer' variable for retrying again at the end
-        wrongAnswer.push(chosenCategory[wordIndex])
+        console.log(chosenCategory[wordIndex]);
         nextSubmit.style.backgroundColor = 'red'
+        // If answer is wrong, it pushes to 'wrong' array to be worked on later
+        wrong.push(chosenCategory[wordIndex])
         resetValues()
     }
 
@@ -69,7 +75,7 @@ function checkAnswer() {
 
 // Resets the flashcard input fields after checking the answer.
 function resetValues() {
-    console.log(chosenCategory[wordIndex].eng, wordIndex);
+    // console.log(chosenCategory[wordIndex].eng, wordIndex);
     userInput.value = ''
     userInput.disabled = true
     userInputSubmit.style.display = 'none'
@@ -77,6 +83,23 @@ function resetValues() {
     wordIndex++
     nextSubmit.addEventListener('click', openFlashcard)
 }
+
+// Takes words in 'wrong' array and reassigns them to 'chosenCatergory' array
+function wrongAnswers() {
+    if (wrong.length > 0) {
+        chosenCategory = []
+        wordIndex = 0
+        wrong.forEach((i) => {
+            chosenCategory.push(i)
+        })
+        wrong = []
+        console.log(chosenCategory);
+        openFlashcard()
+    } else {
+        console.log('Done');
+    }
+}
+
 
 // Helper
 function id(id) {
