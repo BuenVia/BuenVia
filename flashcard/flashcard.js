5b3c77cb521e1f93508922faf4e30007b61c531c
SaracensFan <matthewclifford@hotmail.co.uk>
2022-04-10 18:47:48 +0100

    update

--- flashcard/flashcard.js
+++ flashcard/flashcard.js
@@ -8,29 +8,62 @@ const categoryEl = id('categoryEl')
 const wordEl = id('wordEl')
 const userInput = id('userInput')
 const userInputSubmit = id('userInputSubmit')
+const nextSubmit = id('nextSubmit')
+
+const summaryContainer = id('summaryContainer')
 
 let chosenCategory, wordIndex = 0
 
 // Shows available topics on dashboard
-col.forEach((word) => {
+col.forEach((cat) => {
     const wordDiv = document.createElement('div')
     const wordDet = document.createElement('p')
 
-    wordDet.textContent = word.category
+    wordDet.textContent = cat.category
 
     wordDiv.classList.add('word-list-element')
 
     wordDiv.appendChild(wordDet)
     wordListEl.appendChild(wordDiv)
 
-    chosenCategory = word
-
     //Console Log each word with its category
-    wordDiv.addEventListener('click', openFlashcard)
+    wordDiv.addEventListener('click', () => {
+        chosenCategory = cat.words
+        categoryEl.textContent = `${cat.category}`
+        dashboardContainer.classList.add('hidden')
+        flashcardContainer.classList.remove('hidden')
+        openFlashcard(cat)
+    })
 })
 
+function openFlashcard() {
+    // chosenCategory.forEach((word) => console.log(word))
+    if (wordIndex < chosenCategory.length) {
+        console.log(wordIndex);
+        userInput.disabled = false
+        nextSubmit.style.display = 'none'
+        userInputSubmit.style.display = 'inline'
+        wordEl.textContent = `${chosenCategory[wordIndex].spa}`
+        userInputSubmit.addEventListener('click', checkAnswer)
+    } else {
+        flashcardContainer.classList.add('hidden')
+        summaryContainer.classList.remove('hidden')
+    }
+}
 
-
+function checkAnswer() {
+    if (userInput.value) {
+        console.log(chosenCategory[wordIndex].eng, wordIndex);
+        userInput.value = ''
+        userInput.disabled = true
+        userInputSubmit.style.display = 'none'
+        nextSubmit.style.display = 'inline'
+        wordIndex++
+        nextSubmit.addEventListener('click', openFlashcard)
+    } else {
+        console.log('empty');
+    }
+}
 
 // Helper
 function id(id) {
