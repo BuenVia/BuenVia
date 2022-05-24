5ba9b87e0b30ce754a47efec5d11f64bfda3bf12
SaracensFan <matthewclifford@hotmail.co.uk>
2022-05-24 13:44:11 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,34 +2,43 @@ const verbEl = document.getElementById('verbEl')
 const tenseTitle = document.getElementById('tenseTitle')
 const verbContainer = document.getElementById('verbContainer')
 
+let chosenVerb
+
+// Load in wordBank JSON file
 fetch ('./wordbank.json')
     .then(res => res.json())
     .then(words => getWords(words))
     .catch(err => console.log(err))
 
+// Separate tenses
 function getWords(words) {
     words.forEach(word => {
         createTense(word.tense, word.verbs)
     });
 }
 
+// Render each word and tense to screen
 function createTense(tense, verbs) {
-    const tenseDiv = document.createElement('div')
-    const tenseTitle = document.createElement('h3')
-    
-    
-    tenseDiv.classList.add('tense-container')
-    
-    tenseTitle.innerText = tense
-    tenseDiv.appendChild(tenseTitle)
-    
-    
-    verbs.forEach(verb => {
-        console.log(verb);
-        const pVerb = document.createElement('p')
-        pVerb.innerText = verb.verb
-        tenseDiv.appendChild(pVerb)    
-    })
+    const div = document.createElement('div')
+    const btnDiv = document.createElement('div')
+    div.innerText = tense
+    div.classList.add('tense-container')
 
-    verbEl.append(tenseDiv)
+    div.appendChild(btnDiv)
+    verbEl.appendChild(div)
+
+    verbs.forEach(verb => {
+        const button = document.createElement('button')
+        button.innerText = verb.verb
+        btnDiv.appendChild(button)
+
+        button.addEventListener('click', () => {
+            chosenVerb = {
+                tense: tense,
+                verb: verb.verb,
+                conjugate: verb.conjugate
+            }
+            console.log(chosenVerb.conjugate);
+        })
+    })
 }
