ae01bd2cc307bcf0f60beb4d19f92c32f2134f61
SaracensFan <matthewclifford@hotmail.co.uk>
2022-05-25 14:41:07 +0100

    updat

--- js/conjugate.js
+++ js/conjugate.js
@@ -1,17 +1,32 @@
-const wordObj = {
+const wordObjNew = {
                     tense: "Present",
                     verbs: [{
                                 verb: "Ser",
                                 conjugate: [
-                                        { english: "I am", "spanish": "soy" },        
-                                        { english: "You are", "spanish": "eres" },
-                                        { english: "He / She / It is", "spanish": "es" },
-                                        { english: "We are", "spanish": "somos" },
-                                        { english: "You (all) are", "spanish": "sois" },
-                                        { english: "They are", "spanish": "son" }
+                                        { english: "I am", spanish: "soy" },        
+                                        { english: "You are", spanish: "eres" },
+                                        { english: "He / She / It is", spanish: "es" },
+                                        { english: "We are", spanish: "somos" },
+                                        { english: "You (all) are", spanish: "sois" },
+                                        { english: "They are", spanish: "son" }
                                     ]
                                 }]
                             }
+
+const wordObj = {
+    tense: "Present",
+    verbs: [{
+                verb: "Ser",
+                conjugate: [
+                        { pronoun: "Yo", spanish: "soy" },        
+                        { pronoun: "Tú", spanish: "eres" },
+                        { pronoun: "El / Ella / Usted", spanish: "es" },
+                        { pronoun: "Nosotros", spanish: "somos" },
+                        { pronoun: "Vosotros", spanish: "sois" },
+                        { pronoun: "Ellos / Ellas / Ustedes", spanish: "son" }
+                    ]
+                }]
+            }
 
 const activityContainer = document.getElementById('activityContainer')
 const tenseEl = document.getElementById('tenseEl')
@@ -70,7 +85,7 @@ function setTenseAndVerb() {
 }
 
 function setWord() {
-    wordEl.innerText = wordObj.verbs[0].conjugate[wordIndex].english
+    wordEl.innerText = wordObj.verbs[0].conjugate[wordIndex].pronoun
     nextBtn.style.display = 'none'
     activityContainer.style.backgroundColor = ''
     userInput.value = ''
@@ -80,15 +95,16 @@ function setWord() {
 }
 
 function checkAnswer() {
-    if(userInput.value === '') {
-        alert('Enter something')
-    } else if(userInput.value === wordObj.verbs[0].conjugate[wordIndex].spanish) {
+    // if(userInput.value === '') {
+    //     alert('Enter something')
+    // } else 
+    if(userInput.value === wordObj.verbs[0].conjugate[wordIndex].spanish) {
         activityContainer.style.backgroundColor = 'green'
         userInput.disabled = true
         submitBtn.style.display = 'none'
         nextBtn.style.display = 'block'
         const ansObj = {
-            english: wordObj.verbs[0].conjugate[wordIndex].english,
+            pronoun: wordObj.verbs[0].conjugate[wordIndex].pronoun,
             spanish: wordObj.verbs[0].conjugate[wordIndex].spanish,
             userAns: userInput.value,
             correct: true
@@ -101,7 +117,7 @@ function checkAnswer() {
         submitBtn.style.display = 'none'
         nextBtn.style.display = 'block'
         const ansObj = {
-            english: wordObj.verbs[0].conjugate[wordIndex].english,
+            pronoun: wordObj.verbs[0].conjugate[wordIndex].pronoun,
             spanish: wordObj.verbs[0].conjugate[wordIndex].spanish,
             userAns: userInput.value,
             correct: false
@@ -116,10 +132,10 @@ function summary() {
     summaryContainer.style.display = 'block'
     activityContainer.style.display = 'none'
     correctAnswer.forEach(word => {
-        const colorCor = () => word.correct === true ? 'green' : 'red'
+        const summaryCol = () => word.correct === true ? 'correct' : 'incorrect'
         const summaryBox = `
-            <div class="summaryBox" style="background-color:${colorCor()}">
-               <p>${word.english}</p> 
+            <div class="summaryBox ${summaryCol()} el-box">
+               <p>${word.pronoun}</p> 
                <p>Correct answer:${word.spanish}</p>
                <p>Your answer: ${word.userAns}</p>
             </div>
