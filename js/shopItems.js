dea2cc5304ec2d2b58031e81156122a497b14584
SaracensFan <matthewclifford@hotmail.co.uk>
2022-05-26 18:50:20 +0100

    update

--- js/shopItems.js
+++ js/shopItems.js
@@ -1,57 +1,89 @@
 class shopItem{
-    constructor(item, sku, price, image) {
+    constructor(item, sku, price, image, quantity) {
         this.item = item,
         this.sku = sku,
         this.price = price
         this.image = image
-    }
-
-    addItem() {
-        console.log(true)
+        this.quantity = quantity
     }
 }
 
-const tshirt = new shopItem("T-shirt", 1234, 14.99, 'tshirt.jpg')
-const cap = new shopItem("Baseball Cap", 9876, 8.99, 'cap.jpg')
-const hoodie = new shopItem("Hoodie", 567019, 20.99, 'hoodie.jpg')
-const bag = new shopItem("Bag", 43319, 12.49, 'bag.jpg')
-const coffeeCup = new shopItem("Coffee Cup", 644354, 6.49, 'coffeeCup.jpg')
-const pencilCase = new shopItem("Pencil Case", 9834554, 3.99, 'pencilcase.jpg')
+const tshirt = new shopItem("T-shirt", 1234, 14.99, 'tshirt.jpg', null)
+const cap = new shopItem("Baseball Cap", 9876, 8.99, 'cap.jpg', null)
+const hoodie = new shopItem("Hoodie", 567019, 20.99, 'hoodie.jpg', null)
+const bag = new shopItem("Bag", 43319, 12.49, 'bag.jpg', null)
+const coffeeCup = new shopItem("Coffee Cup", 644354, 6.49, 'coffeeCup.jpg', null)
+const pencilCase = new shopItem("Pencil Case", 9834554, 3.99, 'pencilcase.jpg', null)
 
 const itemsList = [tshirt, cap, hoodie, bag, coffeeCup, pencilCase]
 
 const storeContainer = id('storeContainer')
+const itemViewContainer = id('itemViewContainer')
+const basketContainer = id('basketContainer')
+
+const itemViewAddBtn = id('itemViewAddBtn')
+
+const basketBackBtn = id('basketBackBtn')
+
+const shoppingBasketEl = id('shoppingBasketEl')
+
+const basket = []
 
 
+// Shows items
 itemsList.forEach(item => {
-    
     const html = `
     <div class="item-box">
-    <div class="flex-col-cont">
-    <img src="styles/images/${item.image}" alt="BuenVia Image" class="item-img">
-    </div>
-    <div class="item-details">
-    <h2>${item.item}</h2>
-    <p>£${item.price}</p>
-    <label>Quantity</label>
-    <input type="number" min="1" max="9" placeholder="1">
-    <button data-btn-item="${item.sku}">Add</button>
-    </div>
+        <div class="flex-col-cont">
+            <img src="styles/images/${item.image}" alt="BuenVia Image" class="item-img-sml">
+        </div>
+        <div class="item-details">
+            <h2>${item.item}</h2>
+            <p>£${item.price}</p>
+            <button data-btn-item='${JSON.stringify(item)}'>Buy</button>
+        </div>
     </div>
     `
-
     storeContainer.innerHTML += html
-    
-    document.querySelectorAll('[data-btn-item]').forEach(data_btn => 
-        data_btn.addEventListener('click', () => {
-            console.log(data_btn.dataset)
-        })
-    )
-    
 })
 
+const itemBtn = document.querySelectorAll('[data-btn-item]')
+let itemObj 
 
+//Shows individual item
+itemBtn.forEach(data_btn => 
+    data_btn.addEventListener('click', () => {
+        itemObj = JSON.parse(data_btn.dataset.btnItem)
+        
+        itemViewContainer.classList.remove('hidden')
+        storeContainer.classList.add('hidden')
+        basketContainer.classList.add('hidden')
+            
+        id('itemViewImg').innerHTML = `<img src="styles/images/${itemObj.image}" alt="BuenVia Image" class="itm-img-lg">`
+        id('itemViewTitle').innerText = itemObj.item
+        id('itemViewPrice').innerText = itemObj.price
+    })
+)
+    
+// Add items to basket
+itemViewAddBtn.addEventListener('click', () => {
+    let basketItemObj = {
+        item: itemObj.item,
+        price: itemObj.price,
+        sku: itemObj.sku,
+        quantity: id('itemViewQuantity').value
+    }
+    basket.push(basketItemObj)
+    shoppingBasketEl.innerText = basket.length
 
+    console.log(basket);
+})
+
+// Back button
+basketBackBtn.addEventListener('click', () => {
+    itemViewContainer.classList.add('hidden')
+    storeContainer.classList.remove('hidden')
+})
 
 // Helper
 function id(id) {
