2a4e09e2bce3cb12ef0f4aa794fde0cddbe8357b
SaracensFan <matthewclifford@hotmail.co.uk>
2022-05-29 16:26:42 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const practiceBtn = id('practiceBtn')
 const learnBtn = id('learnBtn')
+const merchBtn = id('merchBtn')
 
 practiceBtn.addEventListener('click', () => {
     window.location.href = "conjugate.html";
@@ -7,6 +8,10 @@ practiceBtn.addEventListener('click', () => {
 
 learnBtn.addEventListener('click', () => {
     window.location.href = "lessons.html"
+})
+
+merchBtn.addEventListener('click', () => {
+    window.location.href = "merch.html"
 })
 
 function id(id) {
